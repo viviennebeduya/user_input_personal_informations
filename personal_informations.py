b36f75a1e255c828a8db99432ca3f50a3a137f89
viviennebeduya <viviennebeduya@gmail.com>
2024-11-30 13:36:09 +0800

    required infos. another person's info

--- personal_informations.py
+++ personal_informations.py
@@ -1,11 +1,31 @@
 with open("./user_informations.txt", "w") as informations:
     while True:
         print("Please provide the following information:")
-        full_name = input ("Full Name: ")
-        age = int(input ("Age: "))
-        address = input ("Address: ")
-        phone_number = input ("Phone Number: ")
-        email = input ("Email Address: ")
+        while True:
+            full_name = input ("Full Name: ")
+            if full_name:
+                break
+            print ("Make sure to fill in this information.")
+        while True:
+            age = input ("Age: ")
+            if age:
+                break 
+            print ("Make sure to fill in this information.")
+        while True:
+            address = input ("Address: ")
+            if address:
+                break 
+            print ("Make sure to fill in this information.")
+        while True:
+            phone_number = input ("Phone Number: ")
+            if phone_number:
+                break 
+            print ("Make sure to fill in this information.")
+        while True:
+            email = input ("Email Address: ")
+            if email:
+                break 
+            print ("Make sure to fill in this information.")
 
         informations.write(
             f"Full Name: {full_name}\n"
@@ -15,7 +35,18 @@ with open("./user_informations.txt", "w") as informations:
             f"Email Address: {email}\n"
         )
 
-        another_person=input("Would you like to input another person's information? (yes/no): ")
-        if another_person != "yes":
-            print ("We have successfully collected all inputs.")
+        informations.write ("\n")
+        
+        while True:
+            another_person=input("Would you like to input another person's information? (yes/no): ")
+            if another_person == "yes":
+                break
+            elif another_person == "no":
+                print ("We have successfully collected all inputs.")
+                break
+            else:
+                print ('Invalid input. Please enter if "yes" or "no"')
+                
+        if another_person == "no":
             break
+            
