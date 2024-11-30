884aa40c75d3059cdce7bb39a01caae76d185090
viviennebeduya <viviennebeduya@gmail.com>
2024-11-30 14:56:16 +0800

    SEARCHED!

--- user_search_from_txt.py
+++ user_search_from_txt.py
@@ -3,9 +3,16 @@ search_input = input ("Please enter the full name to search: ")
 with open ("./user_informations.txt", "r") as informations:
     lines = informations.readlines()
     
+    searched = False
     search_name = []
+
     for line in lines:
-        if search_input in line:
+        if search_input in line: #searched, start collect
+            searched = True
+        if searched: # searched, collect continuous
             search_name.append(line)
+        if searched and not line.strip(): # searched + blank line, end
+            break
+
 
     print (search_name)
