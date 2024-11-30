338b4cdffe979beec9f48cfe4d7e70e2fdaca178
viviennebeduya <viviennebeduya@gmail.com>
2024-11-30 14:13:46 +0800

    user input

--- user_search_from_txt.py
+++ user_search_from_txt.py
@@ -1,3 +1,5 @@
+search_input = input ("Please enter the full name to search: ")
+
 with open ("./user_informations.txt", "r") as informations:
     lines = informations.readlines()
     for line in lines:
