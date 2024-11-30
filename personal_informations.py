686fcb23e57bbd2ce9e002aaf2d71d6efbaebead
viviennebeduya <viviennebeduya@gmail.com>
2024-11-30 16:13:05 +0800

    add regex and other input sensitivities

--- personal_informations.py
+++ personal_informations.py
@@ -1,16 +1,18 @@
+import re
+
 with open("./user_informations.txt", "w") as informations:
     while True:
         print("Please provide the following information:")
         while True:
             full_name = input ("Full Name: ")
-            if full_name:
+            if re.fullmatch(r"[A-Za-z\s]+",full_name): #regex-make sure the input is valid only if the pattern is followed
                 break
-            print ("Make sure to fill in this information.")
+            print ("Make sure to fill this in with valid information (without digits or special characters).")
         while True:
             age = input ("Age: ")
-            if age:
+            if age.isdigit():
                 break 
-            print ("Make sure to fill in this information.")
+            print ("Make sure to fill this in with valid information. Age must be in numbers only.")
         while True:
             birthday = input ("Date of Birth: ")
             if birthday:
@@ -18,24 +20,24 @@ with open("./user_informations.txt", "w") as informations:
             print ("Make sure to fill in this information.")
         while True:
             nationality = input ("Nationality: ")
-            if nationality:
+            if re.fullmatch(r"[A-Za-z\s]+",nationality):
                 break 
-            print ("Make sure to fill in this information.")
+            print ("Make sure to fill this in with valid information.")
         while True:
             address = input ("Address: ")
-            if address:
+            if "City" in address:
                 break 
             print ("Make sure to fill in this information.")
         while True:
-            phone_number = input ("Phone Number: ")
-            if phone_number:
+            phone_number = input ("Phone Number (+63): ")
+            if len(phone_number) == 9 and phone_number.isdigit():
                 break 
-            print ("Make sure to fill in this information.")
+            print ("Make sure to fill this in with valid information.")
         while True:
-            email = input ("Email Address: ")
-            if email:
+            email = input ("Email Address (user@example.com): ")
+            if "@" in email and "." in email:
                 break 
-            print ("Make sure to fill in this information.")
+            print ("Make sure to fill this in with valid information (e.g., user@example.com).")
 
         informations.write(
             f"Full Name: {full_name}\n"
