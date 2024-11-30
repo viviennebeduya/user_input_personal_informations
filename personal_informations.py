b4af8ca9908531430ee4cf2eec6005301dbb0658
viviennebeduya <viviennebeduya@gmail.com>
2024-11-30 11:42:58 +0800

    txt file

--- personal_informations.py
+++ personal_informations.py
@@ -1,6 +1,13 @@
-print("Please provide the following information:")
-full_name = input ("Full Name: ")
-age = input ("Age: ")
-address = input ("Address: ")
-phone_number = input ("Phone Number: ")
-email = input ("Email Address: ")
+with open("./user_informations.txt", "w") as informations:
+    print("Please provide the following information:")
+    full_name = input ("Full Name: ")
+    age = int(input ("Age: "))
+    address = input ("Address: ")
+    phone_number = int(input ("Phone Number: "))
+    email = input ("Email Address: ")
+
+    #print ("Please make sure to input accurate informations.")
+
+    informations.write(
+        f"Full Name: {full_name}\n"
+    )
